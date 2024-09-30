a59a1e9f65f923a1762650dd85e8ce71a179d6c8
Lucas <Lucas@DESKTOP-REP5IP9>
2024-09-30 14:14:22 +0200

    Add project files.

---
+++ TcpJson/Dice.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using TcpOpg1;
+
+public class Dice
+{
+	public int LowPip { get; set; }
+	public int HighPip { get; set; }
+
+	public Dice() { }
+
+	public Dice (int lowPip, int highPip)
+	{
+		LowPip = lowPip;
+		HighPip = highPip;
+	}
+}
---
+++ TcpJson/NumberTable.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TcpJson
+{
+	public class NumberTable
+	{
+		public int FirstNr {  get; set; }
+		public int LastNr { get; set; }
+
+		public NumberTable() { }
+
+		public NumberTable(int firstNr, int lastNr)
+		{
+			FirstNr = firstNr;
+			LastNr = lastNr;
+		}
+	}
+}
